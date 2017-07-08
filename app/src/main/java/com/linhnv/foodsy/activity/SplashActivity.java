e95590738b5c35ddb6faaac32729f46fe1a31f2e
Nguyen Van Linh <mrlinhsg@gmail.com>
2017-07-08 13:13:23 +0700

    Update Splash and map

--- app/src/main/java/com/linhnv/foodsy/activity/SplashActivity.java
+++ app/src/main/java/com/linhnv/foodsy/activity/SplashActivity.java
@@ -55,7 +55,6 @@ public class SplashActivity extends AppCompatActivity{
             getLastLocation();
         }
     }
-
     @SuppressWarnings("MissingPermission")
     private void getLastLocation() {
         mFusedLocationClient.getLastLocation()
